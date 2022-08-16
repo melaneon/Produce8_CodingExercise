745d6942860d298269c7520a3d750fca1d52d328
Melaniia Frolova <90017935+melaneon@users.noreply.github.com>
2022-08-15 21:11:07 -0700

    Add files via upload

---
+++ data/userInputs.js
@@ -0,0 +1,27 @@
+import { getImageListItemBarUtilityClass } from "@mui/material"
+
+export const inputs = [
+    {
+        id: 1,
+        purchaseprice: 250000,
+        interestrate: 1.5,
+        period: 25,
+        monthlypayment: 853.50,
+        name: "Joe",
+        familyName: "Done",
+        phoneNumber: 2504566545,
+        emailAddress: 'lucky@gmail.com'
+
+    },
+    {
+        id: 2,
+        purchaseprice: 50000,
+        interestrate: 1.5,
+        period: 30,
+        monthlypayment: 853.50,
+        name: "Maria",
+        familyName: "Doe",
+        phoneNumber: 2505556677,
+        emailAddress: 'lucky2@gmail.com'
+    },
+]
